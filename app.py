3e1410715009614097c48a96ebd1dd0deefb8136
Dinesh <dineshkumar731999@gmail.com>
2023-12-19 14:49:56 +0530

    games.txt and hotels.txt file is added app.py is added and modified

---
+++ app.py
@@ -0,0 +1,24 @@
+class Prey:
+    def flee(self):
+        print('the Animal is Fleeing')
+
+class Predator:
+    def hunt(self):
+        print('the Animal is Hunting')
+class Rabbit(Prey):
+    pass
+class Hawk(Predator):
+    pass
+class Fish(Prey,Predator):
+    pass
+
+rabbit=Rabbit()
+hawk=Hawk()
+fish=Fish()
+
+rabbit.flee()
+hawk.hunt()
+print()
+print('The Fish Can be both prey and predator ')
+fish.flee()
+fish.hunt()
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Hello Everyone
+
+This is my Git Learning Project 
+
+I have Strated Learning Git and GitHub Tutorials 
+
+
